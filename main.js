3ea1e96612d90c309e1f18fe84d27a8050cb6563
Ciro Neto <ciro.neto16@gmail.com>
2021-08-12 19:09:49 -0300

    organize some JS and CSS code

--- main.js
+++ main.js
@@ -21,13 +21,57 @@ for (const link of links) {
 const header = document.querySelector('#header1')
 const navHeight = header.offsetHeight
 
-function changeHeaderWhenScroll () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
 }
+
+/*back to top button*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY > 2500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*ACTIVED MENU LINK*/
+const sections = document.querySelectorAll('section[id]')
+
+function activeMenuLinkStyleAtRespecitveSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll functions*/
+window.addEventListener('scroll', function () {
+  activeMenuLinkStyleAtRespecitveSection()
+  backToTop()
+  changeHeaderWhenScroll()
+})
 
 /*TESTIMONIAL SWIPER*/
 const swiper = new Swiper('.swiper-container', {
@@ -63,48 +107,3 @@ scrollReveal.reveal(
   `,
   { interval: 100 }
 )
-
-/*back to top button*/
-
-function backToTop (){
-  const backToTopButton = document.querySelector('.back-to-top')
-  if (window.scrollY > 2500) {
-    backToTopButton.classList.add('show')
-  } else {
-    backToTopButton.classList.remove('show')
-  }
-}
-
-/*ACTIVED MENU LINK*/
-const sections = document.querySelectorAll('section[id]')
-
-function activeMenuLinkStyleAtRespecitveSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
-
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
-    }
-  }
-}
-
-
-/* When Scroll functions*/
-window.addEventListener('scroll', function () {
-  activeMenuLinkStyleAtRespecitveSection()
-  backToTop()
-  changeHeaderWhenScroll ()
-})
